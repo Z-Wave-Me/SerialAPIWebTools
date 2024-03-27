bf1095008956b01ccb60d593c2f1d7838571e357
amatilda <kpk@z-wave.me>
2024-03-28 02:59:36 +0300

    add webpack build

--- src/controller_ui.ts
+++ src/controller_ui.ts
@@ -32,7 +32,7 @@ class ControllerUiClass {
 	private readonly MESSAGE_READ_BOARD_INFO:string					= "Read board info the controller";
 	private readonly MESSAGE_SET_REGION:string						= "Set region the controller";
 	private readonly MESSAGE_PLEASE_WAIT:string						= "Please wait until the previous operation is completed.";
-	private readonly MESSAGE_VERSION_LOG							= "SerialAPIWebTools version 0.0.0.1";
+	private readonly MESSAGE_VERSION_LOG							= "SerialAPIWebTools version 0.0.1";
 
 	private readonly TABLE_NAME_SERIAL_API_VERSION:string			= "Serial API Version:";
 	private readonly TABLE_NAME_VENDOR:string						= "Vendor:";
--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,40 @@
 const path = require('path');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
-module.exports = {
-  plugins: [new MiniCssExtractPlugin()],
-  devtool: 'source-map',
-  entry: './src/controller_ui.ts',
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        use: 'ts-loader',
-        exclude: /node_modules/,
-      },
-      {
-        test: /\.html$/i,
-        loader: "html-loader",
-      },
-      {
-        test: /\.scss$/i,
-        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
-      },
-    ],
-  },
-  resolve: {
-    extensions: ['.tsx', '.ts', '.js'],
-  },
-  output: {
-    filename: 'controller_ui.js',
-    libraryTarget: 'umd',
-    library: 'ControllerUiLib',
-    path: path.resolve(__dirname, 'dist')
-  }
+module.exports = function(env, argv) {
+	return {
+		plugins: [new MiniCssExtractPlugin()],
+		devtool: env.production ? 'source-map' : 'eval',
+		entry: './src/controller_ui.ts',
+		module: 
+		{
+			rules:
+			[
+				{
+					test: /\.tsx?$/,
+					use: 'ts-loader',
+					exclude: /node_modules/,
+				},
+				{
+					test: /\.html$/i,
+					loader: "html-loader",
+				},
+				{
+					test: /\.scss$/i,
+					use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+				},
+			],
+		},
+		resolve:
+		{
+			extensions: ['.tsx', '.ts', '.js'],
+		},
+		output:
+		{
+			filename: 'controller_ui.js',
+			libraryTarget: 'umd',
+			library: 'ControllerUiLib',
+			path: path.resolve(__dirname, 'build')
+		}
+	}
 };
