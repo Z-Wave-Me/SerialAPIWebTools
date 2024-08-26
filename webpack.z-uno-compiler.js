be3f8964a5b0362b7876e0ac325f44b2f7b6dd11
amatilda <kpk@z-wave.me>
2024-08-26 17:00:38 +0300

    add generate z-uno-compiler.script

--- webpack.z-uno-compiler.js
+++ webpack.z-uno-compiler.js
@@ -1,5 +1,22 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 const common = require("./webpack.common.js")
 
+const template_html = '<script src="/files/z-uno2/Z-Uno-Compiler{{beta}}/z-uno-compiler.js?{{version}}"></script>'
+
 module.exports = function(env, argv) {
-	return (common.common(env, argv, './src/z-uno-compiler.ts', 'z-uno-compiler.js', 'ZUnoCompiler'));
+	let template_html_replace;
+	
+	if (common.beta == true)
+		template_html_replace = "-beta";
+	else
+		template_html_replace = "";
+	const config = common.common(env, argv, './src/z-uno-compiler.ts', 'z-uno-compiler.js', 'ZUnoCompiler');
+	config.plugins.push(
+		new HtmlWebpackPlugin({
+			inject: false,
+			templateContent:template_html.replace(new RegExp("{{beta}}", 'g'), template_html_replace).replace(new RegExp("{{version}}", 'g'), common.web_tools_version),
+			filename: 'z-uno-compiler.script',
+		}),
+	);
+	return (config);
 };
